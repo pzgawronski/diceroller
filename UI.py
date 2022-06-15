0b09ce081e86e2d528028aa10405825fc9f9a7e4
Patryk Gawronski <pzgawronski@gmail.com>
2022-06-15 16:45:01 +0200

    cleaned up functions + moved constants to a separate file

--- UI.py
+++ UI.py
@@ -1,15 +1,6 @@
 import tkinter as tk
 import random as rd
-
-FONT = ("Lato Semibold", 16, "normal")
-RESULT_FONT = ("Lato Semibold", 32, "normal")
-SIDES = [2, 4, 6, 8, 10, 100, 12, 20]
-MAX_DICE_POOL = 10
-BTN_HEIGHT = 2
-BTN_WIDTH = BTN_HEIGHT * 2
-BTN_WIDE = BTN_HEIGHT * 4
-PAD = 5
-PAD_WINDOW = PAD*3
+from const import *
 
 
 class Roller:
@@ -20,29 +11,16 @@ class Roller:
         self.window.config(width=640, height=480, padx=PAD_WINDOW, pady=PAD_WINDOW)
 
         self.dice_buttons = [DiceButton(self, SIDES.index(side), side) for side in SIDES]
-        self.dice_labels = [Label(column) for column in range(len(SIDES))]
+        self.dice_labels = [DiceLabel(column) for column in range(len(SIDES))]
 
-        self.roll_btn = Button()
-        self.roll_btn.button.config(text="Roll!", width=BTN_WIDE, command=self.roll_dice)
-        self.roll_btn.button.grid(column=2, row=0)
+        self.result = Label(2, 1, RESULT_FONT, "")
+        self.result.label.grid(rowspan=2)
 
-        self.result = Label(len(SIDES))
-        self.result.label.config(font=RESULT_FONT)
-        self.result.label.grid(column=2, row=1, rowspan=2)
-
-        self.adv_btn = Button()
-        self.adv_btn.button.config(text="Adv. d20", width=BTN_WIDE, command=self.advantage)
-        self.adv_btn.button.grid(column=2, row=len(SIDES) - 2)
-
-        self.dadv_btn = Button()
-        self.dadv_btn.button.config(text="dAdv. d20", width=BTN_WIDE, command=self.disadvantage)
-        self.dadv_btn.button.grid(column=2, row=len(SIDES) - 3)
-
-        self.clear_btn = Button()
-        self.clear_btn.button.config(text="Clear", width=BTN_WIDE, command=self.clear)
-        self.clear_btn.button.grid(column=2, row=len(SIDES) - 1)
-
-        self.rolling_buttons = [self.roll_btn, self.adv_btn, self.dadv_btn]
+        self.roll_btn = Button(2, 0, "Roll!", self.roll_dice, True)
+        self.adv_btn = Button(2, ROWS-3, "Adv. d20", self.advantage)
+        self.dadv_btn = Button(2, ROWS-2, "dAdv. d20", self.disadvantage, True)
+        self.clear_btn = Button(2, ROWS-1, "Clear", self.clear, True)
+        self.short_btns = [self.adv_btn, self.dadv_btn]
 
         self.window.mainloop()
 
@@ -50,13 +28,7 @@ class Roller:
         results = []
         for pool in self.dice_labels:
             if pool.label["text"]:
-                sub_results = []
-                dice_int = [int(x) for x in pool.label["text"].split("k")]
-                for _ in range(dice_int[0]):
-                    roll = rd.randrange(dice_int[1])+1
-                    sub_results.append(roll)
-                    results.append(roll)
-                pool.label.config(text=f"{sub_results}")
+                pool.label_to_results(results)
         result = sum(results)
         self.result.label.config(text=f"{result}")
         self._disable_rolling()
@@ -68,35 +40,31 @@ class Roller:
         self._clear_labels()
 
     def _double_d20(self):
+        self.reset_if_needed()
+        self._clear_labels()
         d20_btn = self.dice_buttons[-1]
         d20_label = self.dice_labels[-1]
         for _ in range(2):
             d20_btn.add_die()
         results = []
-        dice_int = [int(x) for x in d20_label.label["text"].split("k")]
-        for _ in range(dice_int[0]):
-            roll = rd.randrange(dice_int[1]) + 1
-            results.append(roll)
-        d20_label.label.config(text=f"{results}")
+        d20_label.label_to_results(results)
         return results
 
     def advantage(self):
         result = max(self._double_d20())
-        self.result.label.config(text=f"{result}")
+        self.result.update(result)
         self._disable_rolling()
 
     def disadvantage(self):
         result = min(self._double_d20())
-        self.result.label.config(text=f"{result}")
+        self.result.update(result)
         self._disable_rolling()
 
     def _disable_rolling(self):
-        for button in self.rolling_buttons:
-            button.button.config(state="disabled")
+        self.roll_btn.button.config(state="disabled")
 
     def _enable_rolling(self):
-        for button in self.rolling_buttons:
-            button.button.config(state="normal")
+        self.roll_btn.button.config(state="normal")
 
     def _disable_dice_btns(self):
         for button in self.dice_buttons:
@@ -121,29 +89,29 @@ class Roller:
         if reset_needed:
             self.clear()
 
-
 class Button:
 
-    def __init__(self):
-        self.button = tk.Button(font=FONT, height=BTN_HEIGHT, width=BTN_WIDTH)
+    def __init__(self, column: int, row: int, text: str, command, wide=True):
+        self.width = BTN_WIDE if wide else BTN_WIDTH
+        self.button = tk.Button(font=FONT, height=BTN_HEIGHT, width=self.width)
+        self.button.config(text=text, command=command)
+        self.button.grid(row=row, column=column, padx=PAD, pady=PAD)
 
 
 class DiceButton(Button):
 
-    def __init__(self, roller: Roller, row: int, sides: int):
-        super().__init__()
+    def __init__(self, roller: Roller, index: int, sides: int):
+        super().__init__(0, index, f"{sides}", self.add_die, wide=False)
         self.roller = roller
-        self.index = row
+        self.index = index
         self.sides = sides
         self.die_counter = 0
-        self.button.config(text=f"{sides}", command=self.add_die)
-        self.button.grid(row=row, column=0, padx=PAD, pady=PAD)
 
     def add_die(self):
         self.roller.reset_if_needed()
         self.die_counter += 1
         die_label = self.roller.dice_labels[self.index].label
-        die_label.config(text=f"{self.die_counter}k{self.sides}")
+        die_label.config(text=f"{self.die_counter}{DICE_MARKER}{self.sides}")
         if self.die_counter == MAX_DICE_POOL:
             die_button = self.roller.dice_buttons[self.index].button
             die_button.config(state="disabled")
@@ -151,6 +119,26 @@ class DiceButton(Button):
 
 class Label:
 
+    def __init__(self, column: int, row: int, font: tuple, text: str):
+        self.label = tk.Label(text=text, font=font, padx=PAD, pady=PAD, width=BTN_WIDE, wraplength=75)
+        self.label.grid(column=column, row=row)
+
+    def update(self, text):
+        self.label.config(text=f"{text}")
+
+
+class DiceLabel(Label):
+
     def __init__(self, row: int):
-        self.label = tk.Label(text="", font=FONT, padx=PAD, pady=PAD, width=BTN_WIDE, wraplength=75)
-        self.label.grid(row=row, column=1)
+        super().__init__(1, row, FONT, "")
+
+    def label_to_results(self, results: list):
+        split_label = self.label["text"].split(DICE_MARKER)
+        dice_int = [int(x) for x in split_label]
+        pool_count, pool_sides = dice_int
+        pool_results = []
+        for _ in range(pool_count):
+            roll = rd.randrange(pool_sides) + 1
+            pool_results.append(roll)
+        self.update(pool_results)
+        results.extend(pool_results)
